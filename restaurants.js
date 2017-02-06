535ee85917becc74567741daba6779168f479742
LudoCarlu <ludoviccarlu@gmail.com>
2017-02-06 00:38:39 -0500

    Insertion de certaines créations HTML via le code js

--- restaurants.js
+++ restaurants.js
@@ -1,11 +1,62 @@
-$.getJSON("restaurants.json", function(data) {
+/*$.getJSON("restaurants.json", function(data) {
     console.log(data);
 
     $("#adr").html(data[0].adr_address);
     $var = document.getElementById('icons');
     $var.src = data[0].icon;
+});*/
+
+/*$(function() {
+    $.getJSON('restaurants.json', function(data) {
+        $.each(data, function(index, d) {
+            $('#contenu').append('Adresse : ' + d.adr_address + '<br>');
+            $('#contenu').append('<img src=' + d.icon + '>');
+        });
+    });
+});*/
+//<p id="adr"> </p>
+//<img id="icons" src="" alt="test">
+
+var extraction_donnees = function() {
+  var d = [];
+  $.getJSON('restaurants.json', function(data) {
+    d.push(data);
+  });
+  return d;
+};
+
+var creation_html = function(data) {
+  data.forEach(function(obj) {
+    for(var i in obj) {
+      var divTmp = document.createElement("div");
+      var idDiv = document.createAttribute("id");
+      idDiv = obj[i].name;
+      divTmp.setAttribute("id",idDiv);
+      var img = ajoutImage(obj[i].icon);
+      var adresse = ajoutAdresse(obj[i].adr_address);
+      divTmp.appendChild(img);
+      divTmp.appendChild(adresse);
+      document.getElementById("contenu").appendChild(divTmp);
+    }
+
+  })
+};
+
+var ajoutImage = function(src) {
+  var img = document.createElement("img");
+  img.src = src;
+  return img;
+}
+//Essayer de split l'adresse avec les </span>
+var ajoutAdresse = function(adr) {
+  var adresse = document.createElement(adr);
+  var t = document.createTextNode(adr);
+  adresse.appendChild(t);
+  return adresse;
+}
 
 
-
-
-});
+var lancement =  function() {
+  var d = extraction_donnees();
+  creation_html(d);
+}
