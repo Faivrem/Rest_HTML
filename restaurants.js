3a5e557a04e6a5c9f52c3830d397666f42c8c3a5
faivrem <max.faivre6@free.fr>
2017-02-11 20:00:22 -0500

    Affichage+ recherche

--- restaurants.js
+++ restaurants.js
@@ -24,29 +24,86 @@ xmlHttp.onreadystatechange = function() {
 xmlHttp.open("GET", localhost, true);
 xmlHttp.send();*/
 
-$('.container-restaurant').click(function(){
-  //$('.container-selected').append(this.id);
-  $('.container-selected #title').text(db[this.id].name);
-  $('.container-selected #address').replaceWith(db[this.id].adr_address);
-  $('.container-selected #img').attr("src",db[this.id].icon);
-  $('.container-selected #tel').text(db[this.id].international_phone_number);
-  var sch;
-  db[this.id].opening_hours.weekday_text.forEach(function (i){
-    sch=i+sch;
-  })
-  $('.container-selected #schedule').text(sch);
-  if(db[this.id].opening_hours.open_now==true){
-    $('.container-selected #schedule').append("OUI");
-  }
-  else {
-    $('.container-selected #schedule').append("NON");
-  }
+$('.container-restaurant').click(function() {
+    $('.container-selected').show();
+    //$('.container-selected').append(this.id);
+    $('.container-selected .title').text(db[this.id].name);
+    $('.container-selected .address').replaceWith(db[this.id].adr_address);
+    $('.container-selected .img').attr("src", db[this.id].icon);
+    $('.container-selected .tel').text(db[this.id].international_phone_number);
 
+    //Schedule
+    var sch;
+    db[this.id].opening_hours.weekday_text.forEach(function(i) {
+        sch = i + sch;
+    })
+    $('.container-selected .schedule').text(sch);
+    if (db[this.id].opening_hours.open_now == true) {
+        $('.container-selected .schedule').append("OUI");
+    } else {
+        $('.container-selected .schedule').append("NON");
+    }
+    $('.container-selected .review').text('');
+    // Reviews
+    db[this.id].reviews.forEach(function(i) {
+        var com = $("<div></div>").addClass('Com');
+        com.addClass('bg-primary');
+        com.append('<img src="https://cdn3.iconfinder.com/data/icons/black-easy/512/538474-user_512x512.png" alt="..." class="img-rounded" height=30px>');
+        com.append("<b>" + i.author_name + "</b><br>");
+        for (var t = 1; t <= 5; t++) {
+            if (t < i.rating) {
+                com.append('★');
+            } else {
+                com.append('☆');
+            }
+        }
+        com.append("<br>" + i.text);
+        com.appendTo('.container-selected .review');
+    })
 
 });
 
+var post = function() {
+    var pseudo = $('#user').val();
+    var com = $('#comment').val();
+
+    var div = $("<div></div>").addClass('Com');
+    div.addClass('bg-primary');
+    div.append('<img src="https://cdn3.iconfinder.com/data/icons/black-easy/512/538474-user_512x512.png" alt="..." class="img-rounded" height=30px>');
+    div.append("<b>" + pseudo + "</b><br>");
+    div.append("<br>" + com);
+    div.prependTo('.container-selected .review');
+
+}
+
+var search = function() {
+    var text = $('#recherche').val();
+    if (text != "") {
+        var words = text.split(" ");
+
+        $('.container-restaurant').each(function() {
+            $(this).hide();
+        })
+        words.forEach(function(i) {
+            var regex = new RegExp(i, 'i');
+            console.log(regex);
+            $('.name').each(function() {
+                if ($(this).text().match(regex)) {
+                    console.log("match");
+                    var div = $(this).parent().parent().parent();
+                    div.show();
+                }
+                console.log($(this).text());
+            })
+
+
+
+        })
+    }
+}
+
 var chargement = function() {
-  var co=0;
+    var co = 0;
     db.forEach(function(obj) {
         var containerRestaurant = document.createElement("div");
         containerRestaurant.className = "container-restaurant col-md-4";
@@ -57,10 +114,11 @@ var chargement = function() {
         var img = document.createElement("img");
         img.src = obj.icon;
         var containerInformations = document.createElement("div");
-        containerRestaurant.setAttribute("id",co);
+        containerRestaurant.setAttribute("id", co);
 
         var containerInformationsAdresse = document.createElement("div");
-        containerInformationsAdresse.innerHTML = obj.adr_address;
+        containerInformationsAdresse.innerHTML = obj.name;
+        containerInformationsAdresse.setAttribute("class", "name");
 
         $(containerRestaurant).append(containerRestaurantSousDiv1);
         $(containerRestaurantSousDiv1).append(img);
@@ -70,11 +128,12 @@ var chargement = function() {
         //var containerRestaurants = document.getElementsByClassName("container-restaurants col-md-8")[0];
         //var containerRestaurants = $(".container-restaurants col-md-8");
         //console.log(containerRestaurants);
-        co=co+1;
+        co = co + 1;
         $(".container-restaurants.col-md-8").append(containerRestaurant);
     })
 }
 chargement();
+$('.container-selected').hide();
 
 /*<div>
     <img src="https://goo.gl/Y5CqQG">
